c9669ee2a00d31cdbe62bfa0a2165e5ecab4d467
Auke van Oostenbrugge <auke.v.o@hotmail.com>
2024-11-14 10:50:24 +0000

    Consistency update II

--- native-federation/remotes/projects/explore/src/bootstrap.ts
+++
@@ -1,9 +0,0 @@
-import { initFederation } from '@angular-architects/native-federation';
-
-
-initFederation()
-  .catch(err => console.error(err))
-  .then(_ => import('./../../recommendations/src/bootstrap'))
-  .catch(err => console.error(err))
-  .then(_ => import('./../../teasers/src/bootstrap'))
-  .catch(err => console.error(err));
