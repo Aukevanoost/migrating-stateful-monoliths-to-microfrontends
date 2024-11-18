72db9372dcf185fb0a405382a3bc561cd7adf29f
Auke van Oostenbrugge <auke.v.o@hotmail.com>
2024-11-18 12:50:09 +0000

    Updated nf

--- mfe/shell/src/loader-with-discovery.js
+++ mfe/shell/src/loader-with-discovery.js
@@ -1,19 +1,22 @@
 import { initFederationWithDiscovery } from 'vanilla-native-federation/plugins/discovery';
 import { createSessionStorageCache } from 'vanilla-native-federation/plugins/storage';
+import { cache } from 'vanilla-native-federation';
 
 (() => {
-    const customCache = createSessionStorageCache({
-        externals: {},
-        remoteNamesToRemote: {},
-        baseUrlToRemoteNames: {},
-        discovery: {}
-    })
+    const customCache = {
+        ...cache.DEFAULT_CACHE,
+        ...createSessionStorageCache({
+            discovery: {}
+        })
+    }
+
     initFederationWithDiscovery(
         "http://localhost:3000", 
         { cache: customCache }
     ).then(({load, discovery, importMap}) => {
-            console.log("discovery: ", discovery);
-            console.log("importMap: ", importMap);
-            window.dispatchEvent(new CustomEvent("mfe-loader-available", {detail: {load}}));
-        })
+        console.log("discovery: ", discovery);
+        console.log("importMap: ", importMap);
+        window.dispatchEvent(new CustomEvent("mfe-loader-available", {detail: {load}}));
+    })
 })();
+
--- mfe/shell/src/loader.js
+++ mfe/shell/src/loader.js
@@ -1,7 +1,12 @@
 import { initFederation } from 'vanilla-native-federation';
-
+import { createSessionStorageCache } from 'vanilla-native-federation/plugins/storage'
 (() => {
-    initFederation("http://localhost:3000/native-federation")
+    const cache = createSessionStorageCache({
+        externals: {},
+        remoteNamesToRemote: {},
+        baseUrlToRemoteNames: {}
+      })
+    initFederation("http://localhost:3000/native-federation", {cache})
         .then(({load, importMap}) => {
             console.log("importMap: ", importMap);
             window.dispatchEvent(new CustomEvent("mfe-loader-available", {detail: {load}}));
