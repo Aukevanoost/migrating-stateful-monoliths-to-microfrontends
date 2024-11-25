f096fe80693b05b0964ef214955dba01807237c6
Auke van Oostenbrugge <auke.v.o@hotmail.com>
2024-11-25 07:17:59 +0000

    Update

--- mfe/discovery/src/routes/index.js
+++ mfe/discovery/src/routes/index.js
@@ -2,7 +2,28 @@ var express = require('express');
 var router = express.Router();
 var {EXPLORE_MFE} = require('./../db/team-explore.js');
 
-router.get('/', function(req, res, next) {
+router.get('/', function(_, res, next) {
+  res.json({
+    ...Object.entries(EXPLORE_MFE)
+      .reduce((remotes, [entry,modules]) => ({
+        ...remotes, 
+        [entry]: modules.reduce((versions, m) => ({
+          ...versions,
+          [m.metadata.version]: {
+            url: m.url,
+            version: m.metadata.version,
+            module: {
+              remoteName: entry,
+              remoteEntry: m.extras.nativefederation.remoteEntry,
+              exposedModule: m.extras.nativefederation.exposedModule
+            }
+          }
+        }), {})
+      }), {})
+  });
+});
+
+router.get('/from-manifest', function(req, res, next) {
   res.json({
     schema: "https://github.com/awslabs/frontend-discovery/blob/main/schema/v1-pre.json",
     microFrontends: {
@@ -16,8 +37,6 @@ router.get('/native-federation', function(req, res, next) {
     ...Object.entries(EXPLORE_MFE).reduce((acc, [k,v]) => {
       return {...acc, [k]: v[0].extras.nativefederation.remoteEntry}
     }, {})
-  }
-
-  );
+  });
 });
 module.exports = router;
