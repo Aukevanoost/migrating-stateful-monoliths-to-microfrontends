77bd70ee5486733d4c454bf6722300c84a683cec
Auke van Oostenbrugge <auke.v.o@hotmail.com>
2024-12-13 14:47:01 +0000

    Rework remoteContentPanel

--- src/main/java/com/aukevanoost/presentation/_core/components/RemoteContentPanel.java
+++ src/main/java/com/aukevanoost/presentation/_core/components/RemoteContentPanel.java
@@ -2,6 +2,8 @@ package com.aukevanoost.presentation._core.components;
 
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.model.LoadableDetachableModel;
+
 import java.io.Serial;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -13,37 +15,39 @@ public class RemoteContentPanel extends Panel {
     @Serial
     private static final long serialVersionUID = 1L;
     private final String remoteUrl;
-    private String content;
+    private static final HttpClient client = HttpClient.newBuilder()
+        .version(HttpClient.Version.HTTP_1_1)
+        .connectTimeout(Duration.ofSeconds(10))
+        .build();
 
     public RemoteContentPanel(String id, String remoteUrl) {
         super(id);
         this.remoteUrl = remoteUrl;
-        setRenderBodyOnly(true);
     }
 
     @Override
     protected void onInitialize() {
         super.onInitialize();
-        content = fetchContent();
-        add(new Label("content", content)
-            .setEscapeModelStrings(false))
-            .setRenderBodyOnly(true);
+
+        add(new Label("content", new LoadableDetachableModel<String>() {
+            @Override
+            protected String load() {
+                return fetchContent();
+            }
+        }).setEscapeModelStrings(false));
     }
 
     private String fetchContent() {
         try {
-            HttpClient client = HttpClient.newBuilder()
-                .version(HttpClient.Version.HTTP_1_1)
-                .connectTimeout(Duration.ofSeconds(10))
-                .build();
-
             HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(remoteUrl))
                 .header("Accept", "text/html")
                 .GET()
                 .build();
 
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            HttpResponse<String> response = client.send(request,
+                HttpResponse.BodyHandlers.ofString());
+
             return response.statusCode() == 200 ? response.body() :
                 "<div class='error'>Error: " + response.statusCode() + "</div>";
         } catch (Exception e) {
--- src/main/java/com/aukevanoost/presentation/home/HomePage.java
+++ src/main/java/com/aukevanoost/presentation/home/HomePage.java
@@ -17,22 +17,29 @@ public class HomePage extends BaseTemplate {
 
     protected void onInitialize() {
         super.onInitialize();
+        System.out.println("RUNNING NEW HOMEPAGE");
         try {
+            long start = System.nanoTime();
+
             var Config = discoveryController.fetchConfig("http://localhost:3000/from-manifest");
+
+            long finish = System.nanoTime();
+            System.out.println("Time taken in ms: " + (finish - start) / 1000000);
 
             String teasersKey = "explore/teasers";
             var teasers = Config.getMicroFrontends().get(teasersKey).getFirst();
             add(new RemoteContentPanel(
                 "exp_teasers",
-                teasers.getExtras().getSsr().getHtml()
+                teasers.getExtras().getSsr().getHtml()  // This is the URL
             ));
 
             String recommendationsKey = "explore/recommendations";
             var recommendations = Config.getMicroFrontends().get(recommendationsKey).getFirst();
             add(new RemoteContentPanel(
                 "exp_recommendations",
-                recommendations.getExtras().getSsr().getHtml()
+                recommendations.getExtras().getSsr().getHtml()  // This is the URL
             ));
+
 
             var hydrationScript = String.format("""
                 import { initMicroFrontends } from './scripts/loader.js';
